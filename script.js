ca1484cff1e74a206ee2dd8e4170e53f62055ddf
DIΛПΛ <89767500+datturbomoon@users.noreply.github.com>
2025-09-24 13:03:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const taskInput = document.getElementById('task-input');
-const addBtn = document.getElementById('add-btn');
-const taskList = document.getElementById('task-list');
+// DOM elements
+const taskInput = document.getElementById("task-input");
+const addBtn = document.getElementById("add-btn");
+const taskList = document.getElementById("task-list");
 
-// adding task event listener
+// Event listener for Enter key on adding a task
 addBtn.addEventListener("click", addTask);
 taskInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") addTask();
@@ -13,17 +14,17 @@ function addTask() {
     const taskText = taskInput.value.trim();
     if (!taskText) return;
 
-    // create task item
+    // create new task item
     const li = document.createElement("li");
     li.className = "task-item";
     li.innerHTML = `
     <label class="task-label">
-            <input type="checkbox" class="task-check">
-            <span class="task-text">${taskText}</span>
-        </label>
-        <button class="delete-btn" aria-label="Delete" title="Delete task">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-        </button>
+        <input type="checkbox" class="task-check">
+        <span class="task-text">${taskText}</span>
+    </label>
+    <button class="delete-btn" aria-label="Delete" title="Delete task">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+    </button>
     `;
 
     // delete button event listener
